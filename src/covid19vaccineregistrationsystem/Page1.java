a4077421f4cf77ba4624bc72e3dc865777fb35fb
Jeremy Chong <63762684+jeremychonggg@users.noreply.github.com>
2021-11-19 18:44:41 +0800

    Add Login, Register, Personnel Login interface in Page1.java

--- src/covid19vaccineregistrationsystem/Page1.java
+++ src/covid19vaccineregistrationsystem/Page1.java
@@ -9,21 +9,62 @@ import javax.swing.JOptionPane;
 
 public class Page1 extends JFrame implements ActionListener{ //Page1 IS-A JFrame 
     public void actionPerformed(ActionEvent e){ //Needed for ActionListener
-        if (e.getSource() == stop){
-            String input = JOptionPane.showInputDialog("Enter password:");
-            if(input.equals("12345")){
-                System.exit(0);
-            } else {
-                JOptionPane.showMessageDialog(stop, "Wrong password!");
+        if (e.getSource() == login){
+            String id = JOptionPane.showInputDialog("Enter your id:");
+            People found = DataIO.checking(id);
+            if(found==null){ //If not found id in database 
+                JOptionPane.showMessageDialog(login,"You haven't registered!");
+            } else{ //If found id in database 
+                // People enter password 
+                String password = JOptionPane.showInputDialog("Enter your password:");
+                //Check if password is valid 
+                if(Integer.parseInt(password) == found.getPassword()){ //id.getPassword()){
+                    System.out.print("Correct Password"); 
+                    Covid19VaccineRegistrationSystem.login = found;
+                    setVisible(false);
+                    Covid19VaccineRegistrationSystem.second.setVisible(true);
+                } else{ //Else password is not valid 
+                    JOptionPane.showMessageDialog(login,"Wrong password!");
+                }
             }
         } else if (e.getSource() == register) {
-            
-        } else if (e.getSource() == login) {
-            
+            String id = JOptionPane.showInputDialog("Enter your Email:");
+            People found = DataIO.checking(id);
+            if(found != null) {
+                JOptionPane.showMessageDialog(register,"The id has been used!");
+            } else {
+                String icPassportNo = JOptionPane.showInputDialog("Enter your IC/Passport No.:");
+                String name = JOptionPane.showInputDialog("Enter your Name:");
+                String state = JOptionPane.showInputDialog("Enter your State:");
+                String postalCode = JOptionPane.showInputDialog("Enter your PostalCode:");
+                String password = JOptionPane.showInputDialog("Enter your Password:");
+                
+                People c = new People(id, icPassportNo, name, state, postalCode, password, null, null); 
+                DataIO.allPeople.add(c); 
+                DataIO.write(); 
+            }
+        } else if (e.getSource() == personnelLogin) {
+            String id = JOptionPane.showInputDialog("Enter your id:");
+//            Customer found = DataIO.checking(name);
+            if(id==null){ //If not found id in database 
+                JOptionPane.showMessageDialog(personnelLogin,"You haven't registered!");
+            } else{ //If found id in database 
+                // People enter password 
+                String password = JOptionPane.showInputDialog("Enter your password:");
+                //Check if password is valid 
+                if(Integer.parseInt(password) == found.getPassword()){ //id.getPassword()){
+                    System.out.print("Correct Password"); 
+                    Covid19VaccineRegistrationSystem.login = found;
+                    setVisible(false);
+                    Covid19VaccineRegistrationSystem.second.setVisible(true);
+                } else{ //Else password is not valid 
+                    JOptionPane.showMessageDialog(personnelLogin,"Wrong password!");
+                }
+            }
         }
     }
     
-    private Button register, login, stop; 
+    private Button login, register, personnelLogin; 
     
     public Page1() {
         setSize(300,100); //Set Window Size (pixel resolution) 
@@ -31,16 +72,17 @@ public class Page1 extends JFrame implements ActionListener{ //Page1 IS-A JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         setLayout(new FlowLayout()); //Arrange Button based on the sequence I add into JFrame (into Window to show from "Left to Right", "Top to Bottom", and "Centre Alignment") 
     
-        register = new Button("Register"); 
-        login = new Button("Login");
-        stop = new Button("Stop"); 
+        login = new Button("login"); 
+        register = new Button("register");
+        personnelLogin = new Button("personnelLogin"); 
         
-        register.addActionListener(this); //'this' is the current object
-        login.addActionListener(this);     //'this' = first (page name) 
-        stop.addActionListener(this);
-        add(register);
-        add(login); 
-        add(stop); 
+        login.addActionListener(this); //'this' is the current object
+        register.addActionListener(this);     //'this' = first (page name) 
+        personnelLogin.addActionListener(this);
+        
+        add(login);
+        add(register); 
+        add(personnelLogin); 
         
         setVisible(true); 
     }
